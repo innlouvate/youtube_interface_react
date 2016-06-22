dce4e286f8f74e386185cd534149b382f1488451
Lou <lfranklin@deloitte.co.uk>
2016-06-22 17:15:50 +0100

    basic youtube setup, incl search

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -6,7 +7,8 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = '${YOUTUBE_KEY}';
+// const API_KEY = '${YOUTUBE_KEY}';
+const API_KEY = 'AIzaSyBC26diOf24bZl5pLhj2HrTlNVpDQ_8eFw';
 
 class App extends Component {
 	constructor(props) {
@@ -17,7 +19,11 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		this.videoSearch('surfboards')
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({
 				videos: videos,
 				selectedVideo: videos[0]
@@ -26,11 +32,15 @@ class App extends Component {
 	}
 
 	render() {
+		const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={videoSearch} />
 				<VideoDetail video={this.state.selectedVideo} />
-				<VideoList videos={this.state.videos} />
+				<VideoList
+					onVideoSelect = {selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
 			</div>
 		);
 	}
